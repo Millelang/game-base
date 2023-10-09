26f1f03e61e8a27ef729b4889c4beb81f1183572
Millelang <millelang2005@gmail.com>
2023-10-09 13:36:04 +0200

    loggbok =>

---
+++ dokumentation/loggbok.md
@@ -0,0 +1,9 @@
+## Loggbok
+
+### 2020-09-07
+* Skapat projektet
+* Skapat en loggbok
+* Skapat en planering
+* Skapat en setup-funktion
+* Skapat en player funktion som gör så att spelaren rör sig
+* Påbörjat en Input-funktion som gör så att spelaren kan hoppa och röra sig
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,5 @@
+
+import Player from './Player.js'
 export default class Game {
   constructor(width, height) {
     this.width = width
@@ -7,13 +9,21 @@ export default class Game {
     this.gameOver = false
     this.gravity = 1
     this.debug = false
+    this.player = new Player(this)
+
+  }
+  draw(context) {
+    this.player.draw(context)
   }
 
   update(deltaTime) {
+    this.player.update(deltaTime)
     if (!this.gameOver) {
       this.gameTime += deltaTime
     }
   }
 
-  draw(context) {}
+  draw(context) {
+    this.player.draw(context)
+  }
 }
